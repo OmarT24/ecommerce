34ce03e772f85b05ee0494f909b87ac0bc6717c8
OmarT24 <omar.joke@gmail.Com>
2022-10-31 10:42:56 +0100

    add model

--- src/main/java/com/csf/ecommerce/model/Product.java
+++ src/main/java/com/csf/ecommerce/model/Product.java
@@ -25,6 +25,37 @@ public class Product {
 	@Column  
 	private float price; 
 	@Column  
-	private int Qte;  
+	private int Qte;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	public int getQte() {
+		return Qte;
+	}
+	public void setQte(int qte) {
+		Qte = qte;
+	}
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", price=" + price + ", Qte=" + Qte + "]";
+	}  
+	
+	
 }
+
 
