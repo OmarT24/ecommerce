93eae11747a29ab0eb34837cc995c426e5907ca2
OmarT24 <omar.joke@gmail.Com>
2022-10-31 10:51:40 +0100

    adding repository

--- src/main/java/com/csf/ecommerce/repository/Productrepository.java
+++ src/main/java/com/csf/ecommerce/repository/Productrepository.java
@@ -1,5 +1,12 @@
 package com.csf.ecommerce.repository;
 
-public interface Productrepository {
 
-}
+import org.springframework.data.repository.CrudRepository;
+
+import com.csf.ecommerce.model.Product;
+  
+//repository that extends CrudRepository  
+public interface Productrepository extends CrudRepository<Product, Integer>  
+{  
+}  
+
